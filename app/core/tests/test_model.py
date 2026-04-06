3bceb84f58f555cebd279457fc44ac1158da86e6
jackonshiundu <jackonshiundu2019@gmail.com>
2026-04-07 00:05:44 +0300

    api

---
+++ app/core/tests/test_model.py
@@ -0,0 +1,63 @@
+"""
+Tests For Models
+"""
+from decimal import Decimal
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from core import models
+
+
+class ModelTests(TestCase):
+    """Test Models"""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sample_emails = [
+            ["test1@example.com", "test1@example.com"],
+            ["TEST2@EXAMPLE.COM", "TEST2@example.com"],
+            ["Test3@Example.Com", "Test3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email, password="sample123"
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating a user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123")
+
+    def test_create_superuser(self):
+        """Test creating a new superuser"""
+        email = get_user_model().objects.create_superuser("test@example.com", "test123")
+        self.assertEqual(email.email, "test@example.com")
+        self.assertTrue(email.is_superuser)
+        self.assertTrue(email.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+
+        user = get_user_model().objects.create_user("test@example.com", "testpass123")
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Sample Recipe Name",
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Sample recipe description",
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
---
+++ app/user/views.py
@@ -0,0 +1,33 @@
+"""
+Views for the user API.
+"""
+
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from .serializers import UserSerializer, AuthTokenSerializer
+
+# Create your views here.
+class CreateUser(generics.CreateAPIView):
+    """Create a new user."""
+
+    serializer_class = UserSerializer
+
+
+class CreateToken(ObtainAuthToken):
+    """Create a new auth token for the user."""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage Authenticated User"""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        "Retrieve and return Authenticated User"
+        return self.request.user
